08ac3413587642fadf380b6d6fe62ae09d211118
nothariharan <nothariharan@gmail.com>
2026-01-17 22:59:24 +0530

    Security: Ignore private API keys and provide example template

--- lib/utils/api_constants.dart
+++
@@ -1,4 +0,0 @@
-class ApiConstants {
-  // Replace the text inside the quotes with the key you copied from Google AI Studio
-  static const String geminiApiKey = "AIzaSyBl_nn3qVjAtpHgwG2yDSo4mSfCjXFxDG8"; 
-}
